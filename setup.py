38c8a2ad7c1a9e2f9a4410cf07c5268c06ee7e9d
IRIS <bebebietyeu0903@gmail.com>
2022-10-01 19:43:19 +0700

    Update setup file

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
-import sys
-import subprocess
+from sys import executable
+from os import system, name
+
+from subprocess import check_call
 import time
+
 def install():
-    print("Installing Requirements Package...")
+    if name == "nt":
+       system('reg add HKEY_CURRENT_USER\Console /v VirtualTerminalLevel /t REG_DWORD /d 0x00000001 /f')
+    print(f"Installing Requirements Package...")
     time.sleep(1)
-    python = sys.executable
-    subprocess.check_call([python, '-m', 'pip', 'install', '-r', 'requirements.txt'])
+    python = executable
+    check_call([python, '-m', 'pip', 'install', '-r', 'requirements.txt'])
     print("Successfully Installed Requirements Package! You're Good To Go")
-    time.sleep(2)
+if __name__ == "__main__":
+    install()
